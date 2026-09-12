667cef6fd8900f4d997bc19c11e380c0482be3e3
Darren Banfi <33876110+boingball@users.noreply.github.com>
2026-09-12 22:32:21 +0100

    Match BASIC loader filename to TAP code name

--- build_tap.py
+++ build_tap.py
@@ -1,2 +1,3 @@
-wrote popcorn_ay_machine.tap: 29227 bytes machine code/data, 28715 bytes events
-VALID [(19, 0, 0), (34, 255, None), (19, 0, 3), (29229, 255, None)] total 29309
+wrote popcorn_ay_machine_fixed.tap: 29227 bytes machine code/data, 28715 bytes events
+[(19, 0, 0), (41, 255, None), (19, 0, 3), (29229, 255, None)] 29316
+00ff000a000eef2022504f50434f524e2220af0d00140010fa20c12033323736380e90000000
